add7a7dc8d415b51e6ef992eec909db0936d21a6
Fernando Falci <falci@falci.me>
2024-04-23 19:44:44 +0200

    chore: font face inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+import inter from 'tailwindcss-font-inter';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [inter],
 };
