a39c2a1d796b0676194344212a64788f8af57975
Fernando Falci <falci@falci.me>
2024-04-25 12:44:56 +0200

    Revert "chore: font face inter"
    This reverts commit add7a7dc8d415b51e6ef992eec909db0936d21a6.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-import inter from 'tailwindcss-font-inter';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [inter],
+  plugins: [],
 };
