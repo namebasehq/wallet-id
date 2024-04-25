8d37a728a857ad7d42e7d60f9d792abfefc22412
Fernando Falci <falci@falci.me>
2024-04-25 12:49:15 +0200

    chore: use font inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
